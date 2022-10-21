8c812498cc499a8821e3ba0cd89a283992f3fd36
yunhyunsung <gustjd5624@naver.com>
2022-10-21 18:01:18 +0900

    장고 11 수업

---
+++ myajax/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from myajax.models import Sangdata
+from django.http.response import HttpResponse
+import json
+
+# Create your views here.
+
+def MainFunc(request):
+    return render(request, 'main.html')
+
+
+
+def ListFunc(request):
+    return render(request, 'List.html')
+
+
+
+
+def ListDbFunc(request):
+    sdata =Sangdata.objects.all()
+    
+    datas =[]
+    for s in sdata:
+        dic = {'code':s.code, 'sang':s.sang, 'su':s.su, 'dan':s.dan}
+        datas.append(dic)
+    print(datas)
+    return HttpResponse(json.dumps(datas), content_type="application/json")
+
+
+
+
+
+
